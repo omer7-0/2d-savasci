298c7852d1d77267680534622347668087263587
Hora <94986797+omer7-0@users.noreply.github.com>
2025-07-04 17:55:31 +0300

    karaktere hareket verildi ve animasyonları oluşturuldu
    karakterin hareketi kodlandı animasyonları bağlandı sağa ve sola dönüşü eklendi

--- figther/Assets/Script/CharacterMove.cs
+++ figther/Assets/Script/CharacterMove.cs
@@ -3,14 +3,70 @@ using UnityEngine;
 public class CharacterMove : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float moveSpeed;
+    private Animator anim;
+
+    private Rigidbody2D rb2d;
+    float moveHorizontal;
+
+    public bool faceingRight;
+
+
     void Start()
     {
+        moveSpeed = 5;
+        moveHorizontal = Input.GetAxis("Horizontal");
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CharacterMovement();
+        CharacterAnimation();
     }
+    void CharacterMovement()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        rb2d.linearVelocity = new Vector2(moveHorizontal * moveSpeed, rb2d.linearVelocity.y);
+
+    }
+
+  
+    void CharacterAnimation()
+    {
+        if (moveHorizontal > 0)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        if (moveHorizontal == 0)
+        {
+            anim.SetBool("isRunning", false);
+        }
+        if (moveHorizontal < 0)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        if (faceingRight==false && moveHorizontal>0)
+        {
+            CharacterFlip();
+        }
+        if (faceingRight == true && moveHorizontal < 0)
+        {
+            CharacterFlip();
+        }
+    }
+    void CharacterFlip()
+    {
+        faceingRight = !faceingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+
+
+
+
 }
